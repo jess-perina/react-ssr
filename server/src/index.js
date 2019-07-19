4ac56c1e22ae89656182a780333210008eb5b011
Jessica Perina <jperina@gmail.com>
2019-07-19 15:39:57 -0700

    feat(routes): set up route matching with react-router-config on the server

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill'
 import express from 'express'
+import { matchRoutes } from 'react-router-config'
+import Routes from './client/Routes'
 import renderer from './helpers/renderer'
 import createStore from './helpers/createStore'
 
@@ -9,6 +11,8 @@ app.use(express.static('public'))
 
 app.get('*', (req, res) => {
   const store = createStore()
+
+  matchRoutes(Routes, req.path)
 
   res.send(renderer(req, store))
 })

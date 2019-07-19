5f36db329328f88750d7a408ad9cc1331d2f6280
Jessica Perina <jperina@gmail.com>
2019-07-18 20:51:36 -0700

    refactor(webpack): remove extra modules from server side bundle

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base.js')
+const webpackNodeExternals = require('webpack-node-externals')
 
 const config = {
   // building bundle for node, not the browser
@@ -13,7 +14,8 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+  externals: [webpackNodeExternals()]
 }
 
 module.exports = merge(baseConfig, config)

15c40143c918abf75dd4d8ec7c872b89a38b6a6b
Jessica Perina <jperina@gmail.com>
2019-07-21 11:31:54 -0700

    fix(data loading): wrap returned loadData promises in new promises that will always resolve so promise.all renders available content even if orriginal promise rejects

--- server/src/index.js
+++ server/src/index.js
@@ -22,6 +22,12 @@ app.get('*', (req, res) => {
 
   const promises = matchRoutes(Routes, req.path).map(({ route }) => {
     return route.loadData ? route.loadData(store) : null
+  }).map(promise => {
+    if (promise) {
+      return new Promise((resolve) => {
+        promise.then(resolve).catch(resolve)
+      })
+    }
   })
 
   Promise.all(promises).then(() => {

d0848e89a5b5d4621a66bc433a2b41e3c0479e08
Jessica Perina <jperina@gmail.com>
2019-07-20 18:37:38 -0700

    style(home page): update text and add styling

--- server/src/client/pages/HomePage.js
+++ server/src/client/pages/HomePage.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <div>HOME Comp</div>
-      <button onClick={() => console.log('hi there')}>Press</button>
+    <div className="center-align" style={{ marginTop: '200px' }}>
+      <h3>Welcome</h3>
+      <p>Check out this server side rendered app</p>
     </div>
   )
 }

176cd1349a3c1d173d3f33c5ac0a3d4eff9d4525
Jessica Perina <jperina@gmail.com>
2019-07-20 17:41:27 -0700

    feat(redux): add auth checking

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -3,3 +3,9 @@ export const fetchUsers = () => async (dispatch, getState, api) => {
   const res = await api.get('/users')
   dispatch({ type: FETCH_USERS, payload: res })
 }
+
+export const FETCH_CURRENT_USER = 'fetch_user'
+export const fetchCurrentUser = () => async (dispatch, getState, api) => {
+  const res = await api.get('/current_user')
+  dispatch({ type: FETCH_CURRENT_USER, payload: res })
+}
